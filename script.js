3ca7d5580528d06e9c390225fb61e8dfd786f194
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-19 12:29:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+  //Write your code here
+  const allStrongs = document.querySelectorAll("strong");
+  console.log(allStrongs);
+  allStrongs.forEach((ele) => {
+    ele.style.color = "rgb(0, 128, 0)";
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  const allStrongs = document.querySelectorAll("strong");
+  console.log(allStrongs);
+  allStrongs.forEach((ele) => {
+    ele.style.color = "rgb(0, 0, 0)";
+  });
 }
